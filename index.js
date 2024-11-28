f7f99fc97198c651c814fd90b9a4b32d389978fe
Mohammeadh <130226741+Mohammeadh@users.noreply.github.com>
2024-11-28 16:37:39 +0530

    index.js

--- index.js
+++ index.js
@@ -10,11 +10,12 @@ const server = http.createServer(app);
 const frontend_url="https://video-chat-mohammed.netlify.app/" 
 const io = new Server(server, {
   cors: {
-    origin: '*',
+    origin: 'https://video-chat-mohammed.netlify.app',  // Specify the correct frontend URL
     methods: ['GET', 'POST'],
   },
   transports: ['polling', 'websocket'], // Ensure both transports are allowed
-}); 
+});
+
 
 app.use(cors());
 
